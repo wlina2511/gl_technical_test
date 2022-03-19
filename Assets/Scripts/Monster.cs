f2dc1c8171f42153393394f1147193ded25c46ed
williaml-voodoo <william.lina2511@gmail.com>
2022-03-19 10:11:47 +0100

    Added Monsters and Swpaners

---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+
+    public GameObject target;
+    public float health, atkSpeed, moveSpeed;
+    public int level;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        atkSpeed = 0.5f * level;
+        moveSpeed = 0.5f * level;
+        health = 5 * level;
+
+        target = GameObject.FindGameObjectWithTag("Castle");
+
+        transform.localScale *= level * 0.5f;
+
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        // Check if the position of the cube and sphere are approximately equal.
+        if ( Vector3.Distance(transform.position, target.transform.position) > 1f)
+        {
+            float step = moveSpeed * Time.deltaTime; // Calculate distance to move
+            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, step);
+        }
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public float timeBetweenSpawn;
+    private float currentTimeBetweenSpawn;
+
+    public int maxLevel;
+
+    public Transform[] spawners;
+
+    public GameObject monsterPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTimeBetweenSpawn = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentTimeBetweenSpawn > timeBetweenSpawn)
+        {
+            Spawn(spawners[Random.Range(0,spawners.Length)].position, Random.Range(1,maxLevel + 1));
+            currentTimeBetweenSpawn = 0;
+        }
+        else
+        {
+            currentTimeBetweenSpawn += Time.deltaTime;
+        }
+    }
+
+    private void Spawn(Vector3 pos, int level)
+    {
+        GameObject monster = Instantiate(monsterPrefab, pos, Quaternion.identity);
+        monster.transform.position = monster.transform.position + new Vector3(0, monster.transform.localScale.y, 0);
+        monster.GetComponent<Monster>().level = level;
+    }
+}
