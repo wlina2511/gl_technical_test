ca31b441f607e0b36643eaecc5995c4f85064877
williaml-voodoo <william.lina2511@gmail.com>
2022-03-24 09:32:32 +0100

    LegendaryItem

---
+++ Assets/Pop.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pop : MonoBehaviour
+{
+
+    Vector3 startSize;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startSize = this.transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //this.transform.localScale += Vector3.one;
+        this.transform.localScale = (Mathf.PingPong(Time.time/4, 0.1f) +1) * startSize;
+    }
+}
